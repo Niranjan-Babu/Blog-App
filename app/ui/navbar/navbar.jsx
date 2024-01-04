6c699596f175873512aa4f0cb63db1f2485091eb
Niranjan <babuniranjan310@gmail.com>
2024-01-04 17:31:17 +0530

    new build

--- app/ui/navbar/navbar.jsx
+++ app/ui/navbar/navbar.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import styles from './navbar.module.css'
 import Link from 'next/link'
 import Search from '../search/search'
+import { Suspense } from 'react'
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}><b>Blog.io</b></h1>
       
       <div className={styles.navbox}> 
+      <Suspense>
         <Search className={styles.searchbar} placeholder="Search"/>
+      </Suspense>
          <Link href="/"><div className={styles.navbox1}>Home</div></Link> 
          <Link href="/main"><div className={styles.navbox1}>Blogs</div></Link>
          <Link href="https://dashboard-flax-eta.vercel.app/dashboard" target='blank'><div className={styles.navbox1}>Dashboard</div></Link>
